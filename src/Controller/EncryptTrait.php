51d70502302956a37d6bacdb010bfcb2d5d1379a
github-ci <you@example.com>
2025-06-12 05:36:04 +0800

    Exception => Throwable

--- src/Controller/EncryptTrait.php
+++ src/Controller/EncryptTrait.php
@@ -40,7 +40,7 @@ trait EncryptTrait
             }
 
             return (array) $content;
-        } catch (\Exception $e) {
+        } catch  (\Throwable $e) {
             throw new BadRequestException(sprintf('Invalid message content:(%s) %s', $e->getCode(), $e->getMessage()), $e->getCode());
         }
     }
